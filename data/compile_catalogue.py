aaa012120cbcba6172a39c002a7dca93c42150dd
padraicmoran <padraic.moran@universityofgalway.ie>
2023-06-06 17:38:45 +0100

    Compile individual MS files into one index file

---
+++ data/compile_catalogue.py
@@ -0,0 +1,27 @@
+# Compiles the collections of individual MS files in /data/mss into one file /data/mss.xml.
+# Run this file after updating individual manuscripts.
+
+import os
+import sys
+from xml.etree.ElementTree import Element, ElementTree
+
+# load master file
+sourcePath = "./data/mss/"
+targetFile = "mss.xml"
+
+# prepare new output file
+outputTree = ElementTree()
+outputTree._setroot(Element("document"))
+outputRoot = outputTree.getroot()
+
+# compile component files
+mss = os.listdir(sourcePath)
+mss.sort()
+for ms in mss:
+    msTree = ElementTree().parse(sourcePath + ms)
+    outputRoot.append(msTree)        
+
+# sort output file
+# write output file
+outputTree.write("./data/mss.xml", encoding='utf-8', xml_declaration=True)    
+print("Done.")
