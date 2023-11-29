6189ed1ef7b06ffc6d4ba00d9a548137492a952e
padraicmoran <padraic.moran@universityofgalway.ie>
2023-11-29 22:22:53 +0000

    Content updates

--- pages/home.php
+++ pages/home.php
@@ -51,3 +51,33 @@ mapLibraries($xml_mss);
 
 	</div>
 </div>
+
+<div class="col-lg-9">
+
+	<h3>Linked Open Data</h3>
+
+<p>This resource aims to conform as fully as possible to <a href="https://www.go-fair.org/fair-principles/">FAIR Principles</a>, making digital assets 
+Findable, Accessible, Interoperable, and Reuseable.
+</p>
+
+<ul>
+<li>Persistent URLs (URIs) are supplied to identify manuscripts, texts, people and places. 
+</li>
+
+<li>All data is available to 
+   download in XML format (from this site and on <a href="https://github.com/padraicmoran/MIrA/tree/master/data">GitHub</a>).
+   </li>
+
+<li>Wherever possible, data is linked to other Linked Open Data resources.
+</li>
+
+<li>Data may be reused under the following Creative Commons licence: </br>
+<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+<img class="mb-2" style="width: 200px" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-sa.png" alt="CC BY-NC-SA" />
+</a></li>
+
+<li>Work is in progress to make data accessible in machine-readable format (probably RDF).
+   </li>
+</ul>
+
+</div>
